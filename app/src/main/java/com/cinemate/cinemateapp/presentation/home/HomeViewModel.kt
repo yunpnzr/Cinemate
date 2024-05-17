46d2df1dd75105733da10f2883e4947e10e6d1ad
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2024-05-17 17:59:51 +0700

    Fixing detail favorite in home

--- app/src/main/java/com/cinemate/cinemateapp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/cinemate/cinemateapp/presentation/home/HomeViewModel.kt
@@ -1,15 +1,20 @@
 package com.cinemate.cinemateapp.presentation.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.cinemate.cinemateapp.data.model.Movie
+import com.cinemate.cinemateapp.data.model.MovieDetail
+import com.cinemate.cinemateapp.data.repository.detail.DetailMovieRepository
 import com.cinemate.cinemateapp.data.repository.favorite.FavoriteRepository
 import com.cinemate.cinemateapp.data.repository.movie.MovieRepository
+import com.cinemate.cinemateapp.utils.ResultWrapper
 import kotlinx.coroutines.Dispatchers
 
 class HomeViewModel (
     private val movieRepository: MovieRepository,
     private val repo: FavoriteRepository,
+    private val detailMovieRepository: DetailMovieRepository
 ) : ViewModel() {
     fun getMovieNowPlaying() = movieRepository.getNowPlaying(page = 1).asLiveData(Dispatchers.IO)
     fun getMoviePopular() = movieRepository.getPopular(1).asLiveData(Dispatchers.IO)
@@ -21,4 +26,8 @@ class HomeViewModel (
     fun checkMovieList(favoriteId: Int?) = repo.checkFavoriteById(favoriteId).asLiveData(Dispatchers.IO)
 
     fun removeFavoriteById(favoriteId: Int?) = repo.removeFavoriteById(favoriteId).asLiveData(Dispatchers.IO)
+
+    fun getCoverPhoto(id: Int): LiveData<ResultWrapper<MovieDetail>> {
+        return detailMovieRepository.detailMovies(id).asLiveData(Dispatchers.IO)
+    }
 }
